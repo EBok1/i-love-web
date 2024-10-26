c25bf02116e8796276d0b61967b59f783d588523
EBok1 <144004887+EBok1@users.noreply.github.com>
2024-10-26 16:04:56 +0200

    tekst

--- src/_data/weather.js
+++ src/_data/weather.js
@@ -1,13 +1,11 @@
 import EleventyFetch from "@11ty/eleventy-fetch";
 
 export default async function () {
-  // Replace with your Visual Crossing API key and the desired location
-  // const apiKey = "GKVDDNDB62J9SLLU64PB2FENM";
-  // const location = "Enkhuizen"; // You can change this to any location
+// Api key en locatie worden in een keer in de link aagesproken.
   const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/enkhuizen?unitGroup=metric&key=GKVDDNDB62J9SLLU64PB2FENM&contentType=json`;
   return EleventyFetch(url, {
-    duration: "0s",
-    type: "json",
+    duration: "0s", //dit betekend dat de data elke keer wordt ingeladen, als heir 1h staat wordt dit elk uur ververst.
+    type: "json", //dit geeft aan in wat voor taal het is geschreven.
     verbose: true
   })
 };
